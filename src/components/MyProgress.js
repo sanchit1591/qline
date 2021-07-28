abbbbbf982662eb0acb39a7e789d96931cf732ae
sanchit1591 <68103302+sanchit1591@users.noreply.github.com>
2021-07-28 13:41:22 -0700

    DRAFT 1

---
+++ src/components/MyProgress.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CardPage from './CardPage.js'
+
+export default function MyProgress(props) {
+    return (
+        <div>
+            {Object.keys(props.res)
+                .map( (key) => 
+                    (key === props.name) ? <CardPage task={props.task} updated={props.updated} name={key} res={props.res} isMe={true} /> :
+                        null)}
+            {Object.keys(props.res)
+                .map( (key) => 
+                    (key !== props.name) ? <CardPage task={props.task} updated={props.updated} name={key} res={props.res} isMe={false} /> :
+                        null)}
+        </div>
+    )
+}
