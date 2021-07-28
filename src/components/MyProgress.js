6dad70f6a55b1aae72563f531f03af1c68be257b
Sanchit Agarwal <sanchit1591@g.ucla.edu>
2021-07-28 14:33:16 -0700

    draft1

--- src/components/MyProgress.js
+++ src/components/MyProgress.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import CardPage from './CardPage.js'
+import React, { useState, useEffect } from 'react';
+import CardPage from './CardPage.js';
+import axios from 'axios';
 
 export default function MyProgress(props) {
+
+    const [score, setScore] = useState({});
+    const user = {
+        name: props.name,
+        task: props.task
+      } 
+    
+      useEffect(() => {
+        axios.post('https://qline-api21.herokuapp.com/get_scores', user)
+        .then(res => {
+            setScore(res.data[user.name]);
+        });
+
+        console.log("dfs");
+        console.log(score)
+        
+      }, [])
+
+
+    
     return (
         <div>
             {Object.keys(props.res)
                 .map( (key) => 
-                    (key === props.name) ? <CardPage task={props.task} updated={props.updated} name={key} res={props.res} isMe={true} /> :
+                    (key === props.name) ? <CardPage score={score[key]} task={props.task} updated={props.updated} name={key} res={props.res} isMe={true} /> :
                         null)}
+                        {console.log(score[props.name])}
             {Object.keys(props.res)
                 .map( (key) => 
-                    (key !== props.name) ? <CardPage task={props.task} updated={props.updated} name={key} res={props.res} isMe={false} /> :
+                    (key !== props.name) ? <CardPage score={score[key]} task={props.task} updated={props.updated} name={key} res={props.res} isMe={false} /> :
                         null)}
         </div>
     )
